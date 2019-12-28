f2846f4f0ffec5cb8ea0a74ef8e5acde833980d3
seongbeom.jeong <jsacc0619@gmail.com>
2019-12-28 19:47:30 +0900

    프로젝트 스켈레톤

---
+++ src/test/java/org/gam/planthabit/PlanthabitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gam.planthabit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanthabitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
